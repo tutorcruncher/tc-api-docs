b709adb81fec628608ca4ee48fadce009d6ce0ae
Alex Ford <alextford1@gmail.com>
2020-03-31 18:27:15 +0100

    scroll menu updater

--- theme/js/api.js
+++ theme/js/api.js
@@ -12,6 +12,7 @@ function change_menu_tab() {
   if (window.location.hash) {
     set_api_menu_active(window.location.hash);
   } else {
+    window.location.hash = '#introduction';
     set_api_menu_active('#introduction');
   }
 }
@@ -53,9 +54,28 @@ function menu_toggle() {
   });
 }
 
+function scroll_heading_change() {
+  let current_hash = window.location.hash;
+  $('body').scroll(() => {
+    $('.api-section-heading, .api-subsection').each((i, el) => {
+      const $el = $(el);
+      const top = window.pageYOffset;
+      const distance = top - $el.position().top;
+      const hash = $el.attr('id');
+      const padding = 30;
+      if (hash && current_hash !== hash && distance < padding && distance > -padding) {
+        window.history.pushState(null, null, `#${hash}`);
+        current_hash = hash;
+        change_menu_tab();
+      }
+    });
+  });
+}
+
 if (window.jQuery) {
   menu_toggle();
   change_menu_tab();
+  scroll_heading_change();
 
   $(window).bind('hashchange', () => {
     change_menu_tab();
