797fe5aaf4f844519e34a33b670c84909f27bdbd
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2020-04-07 13:08:40 +0100

    Add create a pfi

---
+++ pages/proforma-invoices/create-a-pfi.py
@@ -0,0 +1,11 @@
+import requests
+
+headers = {'Authorization': 'token <API KEY>'}
+client_id = 123
+data = {
+    'amount': 120,
+    'client': client_id,
+    'send_pfi': True,
+}
+r = requests.post('https://secure.tutorcruncher.com/api/clients/', json=data, headers=headers)
+print(r.content.decode())
