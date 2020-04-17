b4c4d40cd4ae4b1fa198f13f2cd9623dc68764c0
Alex Ford <alextford1@gmail.com>
2020-04-17 10:29:24 +0100

    tc.com/api/ fix

--- extensions.py
+++ extensions.py
@@ -1,6 +1,8 @@
 from html import escape
 
+from harrier.config import Mode
 from harrier.extensions import template
+from harrier.render import resolve_url as harrier_resolve_url
 
 
 @template.contextfunction
@@ -22,3 +24,12 @@ def replace_spaces(line):
 def json_line_list(ctx, file_name):
     with open('./pages/' + file_name, 'r+') as f:
         return map(replace_spaces, f.readlines())
+
+
+@template.contextfunction
+def resolve_url(ctx, path):
+    # Need to reference api.tc.com for tc.com/api/
+    config = ctx['config']
+    if config.mode == Mode.development:
+        return config.site_root_dev + harrier_resolve_url(ctx, path)
+    return config.site_root + harrier_resolve_url(ctx, path)
