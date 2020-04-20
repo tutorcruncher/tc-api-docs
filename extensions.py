3db0d4f043b68ab07d705b9a1737f644d0585e40
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2020-04-20 12:39:22 +0100

    JS Fix

--- extensions.py
+++ extensions.py
@@ -6,8 +6,17 @@ from harrier.render import resolve_url as harrier_resolve_url
 
 
 @template.contextfunction
+def resolve_url(ctx, path):
+    # Need to reference api.tc.com for tc.com/api/
+    config = ctx['config']
+    if config.mode == Mode.development:
+        return config.site_root_dev + harrier_resolve_url(ctx, path)
+    return config.site_root + harrier_resolve_url(ctx, path)
+
+
+@template.contextfunction
 def js_url(ctx, path):
-    return path if Mode.development else resolve_url(ctx, path)
+    return path if ctx['config'].mode == Mode.development else harrier_resolve_url(ctx, path)
 
 
 @template.contextfunction
@@ -29,12 +38,3 @@ def replace_spaces(line):
 def json_line_list(ctx, file_name):
     with open('./pages/' + file_name, 'r+') as f:
         return map(replace_spaces, f.readlines())
-
-
-@template.contextfunction
-def resolve_url(ctx, path):
-    # Need to reference api.tc.com for tc.com/api/
-    config = ctx['config']
-    if config.mode == Mode.development:
-        return config.site_root_dev + harrier_resolve_url(ctx, path)
-    return config.site_root + harrier_resolve_url(ctx, path)
