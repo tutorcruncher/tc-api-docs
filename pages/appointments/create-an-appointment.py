3fde13280adbe170097ed0548a21eff0aa20c3ed
Alex Ford <alextford1@gmail.com>
2020-06-17 17:37:05 +0100

    initial, json response missing

---
+++ pages/appointments/create-an-appointment.py
@@ -0,0 +1,26 @@
+import requests
+
+headers = {'Authorization': 'token <API KEY>'}
+data = {
+    "start": "2020-01-01T12:00:00Z",
+    "finish": "2020-01-01T14:00:00Z",
+    "topic": "Lesson 1",
+    "location": None,
+    "extra_attrs": {},
+    "rcras": [
+        {
+            "recipient": 23,
+            "charge_rate": "100.00"
+        }
+    ],
+    "cjas": [
+        {
+            "contractor": 56,
+            "pay_rate": "81.00"
+        }
+    ],
+    "status": "planned",
+    "service": 23,
+}
+r = requests.post('https://secure.tutorcruncher.com/api/appointment/', json=data, headers=headers)
+print(r.content.decode())
