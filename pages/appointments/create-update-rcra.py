5ba0388a00a45929a310e6a1e94736057da762c9
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2021-03-29 09:46:12 +0200

    Update with new rcra status

--- pages/appointments/create-update-rcra.py
+++ pages/appointments/create-update-rcra.py
@@ -3,7 +3,7 @@ import pprint, requests
 headers = {'Authorization': 'token <API KEY>'}
 data = {
     "recipient": 23,
-    "charge_rate": "100.00"
+    "charge_rate": "100.00",
 }
 r = requests.post('https://secure.tutorcruncher.com/api/appointments/<id>/recipient/add/', json=data, headers=headers)
 pprint.pprint(r.json())
