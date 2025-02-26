3cd0f96e04f428c859390fb314aebff45e6a5f20
Henty <traill.c.a.henry@gmail.com>
2025-02-26 12:18:51 +0000

    Job change docs

---
+++ pages/services/remove-cj.py
@@ -0,0 +1,8 @@
+import pprint, requests
+
+headers = {'Authorization': 'token <API KEY>'}
+data = {
+    "contractor": 43,
+}
+r = requests.post('https://secure.tutorcruncher.com/api/services/<id>/contractor/remove/', json=data, headers=headers)
+pprint.pprint(r.json())
---
+++ pages/tenders/reject-a-tender.md
@@ -0,0 +1,4 @@
+### Reject a tender
+
+Rejects an existing tender, removing the related contractor from the Service. Functions the same as if a job application
+was rejected through the UI.
