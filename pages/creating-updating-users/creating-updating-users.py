5ea1cfa540ebfcecd1cd7a4ec8b32897197fce59
Sandeep <sunthapa1@hotmail.com>
2025-04-09 23:45:33 +0100

    dan comments

--- pages/creating-updating-users/creating-updating-users.py
+++ pages/creating-updating-users/creating-updating-users.py
@@ -1,13 +1,14 @@
 import requests
 
 headers = {'Authorization': 'token <API KEY>'}
+
 data = {
-    'user': {
-        'email': 'billy_bob@example.com',
-        'last_name': 'Bob',
-        'photo': 'https://picsum.photos/200/300'
-        # ...
-    },
-    # ...
+  "id": 568433,
+  "email": "billy_bob@example.com",
+  "last_name": "Bob",
+  "mobile": "07842 485 204",
+  "photo": "https://photo_url.com/200/300"
+   # ...
 }
+
 requests.post('...', json=data, headers=headers)
