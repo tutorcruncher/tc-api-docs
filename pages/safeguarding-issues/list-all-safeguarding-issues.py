0173809d86fcf702d6f537a0be537cdd60cbcb2e
DanJezeph15 <danieljezeph@hotmail.co.uk>
2025-04-14 17:02:28 +0100

    Adding Safeguarding Issues to the API docs

---
+++ pages/safeguarding-issues/list-all-safeguarding-issues.py
@@ -0,0 +1,5 @@
+import pprint, requests
+
+headers = {'Authorization': 'token <API KEY>'}
+r = requests.get('https://secure.tutorcruncher.com/api/safeguarding_issues/', headers=headers)
+pprint.pprint(r.json())
