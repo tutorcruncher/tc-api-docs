ac327480412c67d139edb325eafabfe8c4f2bafa
Henty <traill.c.a.henry@gmail.com>
2025-05-02 08:47:36 +0100

    update actions

--- pages/subject-categories/get-a-subject-category.py
+++ pages/subject-categories/get-a-subject-category.py
@@ -1,5 +1,5 @@
 import pprint, requests
 
 headers = {'Authorization': 'token <API KEY>'}
-r = requests.get('https://secure.tutorcruncher.com/api/countries/<id>/', headers=headers)
+r = requests.get('https://secure.tutorcruncher.com/api/subject-categories/<id>/', headers=headers)
 pprint.pprint(r.json())
