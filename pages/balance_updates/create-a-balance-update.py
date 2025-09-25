dbfb379afeaa57ff4cf0e749d9c1ff82eb0530e1
DanJezeph15 <danieljezeph@hotmail.co.uk>
2025-09-25 12:39:07 +0100

    Add send_receipt details in

--- pages/balance_updates/create-a-balance-update.py
+++ pages/balance_updates/create-a-balance-update.py
@@ -6,6 +6,7 @@ data = {
     "credit_amount": 100,
     "method": "cash",
     "description": "Client paid by cash.",
+    "send_receipt": True,
 }
 response = requests.post('https://secure.tutorcruncher.com/api/balance_updates/', json=data, headers=headers)
 pprint.pprint(response.json())
