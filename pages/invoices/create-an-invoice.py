e456ebcd8d57e5881aaf47711ae3b3c664c4a057
Tom Hamilton Stubber <tomhamiltonstubber@gmail.com>
2026-01-30 15:37:52 +0000

    Add new AHC docs

--- pages/invoices/create-an-invoice.py
+++ pages/invoices/create-an-invoice.py
@@ -1,10 +1,12 @@
 import pprint, requests
 
-headers = {'Authorization': 'token <API KEY>'}
+headers = {"Authorization": "token <API KEY>"}
 data = {
-    'appointments': [1234, 1235],
-    'adhoc_charges': [567],
-    'raise_behaviour': 'raise-and-send'
+    "appointments": [1234, 1235],
+    "adhoc_charges": [567],
+    "raise_behaviour": "raise-and-send",
 }
-r = requests.post('https://secure.tutorcruncher.com/api/invoices/', json=data, headers=headers)
+r = requests.post(
+    "https://secure.tutorcruncher.com/api/invoices/", json=data, headers=headers
+)
 pprint.pprint(r.json())
